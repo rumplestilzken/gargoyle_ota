430b71ed5fd9acb252bc8726ea84b70132ee0672
rumplestilzken <rumplestilzken@gmail.com>
2023-09-08 11:59:53 -0400

    Initial Commit

---
+++ update.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -ex
+
+vers="$1"
+
+if [ -z "$vers" ];then
+    echo "Usage: $0 <github version name>"
+    exit 1
+fi
+
+rm -f ota-list.json
+minUtc=""
+for i in ~/dev/external_development/rumplestilzken/ota/*.xz;do
+
+    # Grab props
+    unxz -dc $i |strings |grep -E -e ro.system.build.fingerprint= -e ro.build.flavor= -e ro.system.build.date.utc= > tmpprops
+
+    utc=$(sed -n -E -e 's/ro.system.build.date.utc=//p' tmpprops)
+    flavor=$(sed -n -E -e 's/ro.build.flavor=//p' tmpprops |sed -E 's/-user.*//g')
+    if echo "$i" |grep -qE secure;then
+        flavor="$flavor-secure"
+    elif echo "$i" |grep -qE vndklite;then
+        flavor="$flavor-vndklite"
+    fi
+
+    if [ "$minUtc" == "" ] || [ "$utc" -le "$minUtc" ];then
+        minUtc="$utc"
+    fi
+    size="$(du -s "$i" |grep -oE '^[0-9]+')"
+    url=https://github.com/rumplestilzken/gargoyle_lineageos20/releases/download/"$vers"/"$(basename "$i")"
+    cat >> ota-list.json << EOF
+{ "name": "$flavor", "size": $size, "url": "$url"},
+EOF
+done
+
+# Add a stupid entry to not have to remove the trailing `,`
+echo '{ "name": "useless", "size": 0, "url": "http://devnull.org"}' >> ota-list.json
+
+# Optimism
+minUtc=$((minUtc-100))
+
+cat > ota.json << EOF
+{
+    "version": "$vers",
+    "date": "$minUtc",
+    "variants": [
+EOF
+cat ota-list.json >> ota.json
+
+cat >> ota.json << EOF
+    ]
+}
+EOF
